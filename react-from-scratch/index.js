55ef6a405af2286d84d624f634272bd12bbac7da
Marc Hildenbrand <mwhildenbrand@gmail.com>
2025-08-22 06:26:28 +0000

    Finished

--- react-from-scratch/index.js
+++ react-from-scratch/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-
-const App = () => {
-  return <h1>This is a React Script!</h1>
-}
+import App from './App.jsx';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
-root.render(<App />)
+root.render(<App message="Hello from the App component!" />)
